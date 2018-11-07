4ef98b1f802a168da2af782b03c034a61c82275c
sohei.nobe <sohei.nobe@rakus.co.jp>
2018-11-07 14:31:54 +0900

    (1-2)メンバーフォームのインデントを整えた

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberForm.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberForm.java
@@ -14,16 +14,16 @@ import org.hibernate.validator.constraints.NotBlank;
 public class MemberForm {
 	/** 名前 */
 	@NotBlank(message = "名前は必須です。")
-	@Size(min=4,max=10,message="名前は４文字以上、１０文字以下で設定してください。")
+	@Size(min = 4, max = 10, message = "名前は４文字以上、１０文字以下で設定してください。")
 	private String name;
 	/** メールアドレス */
 	@NotBlank(message = "メールアドレスは必須です。")
 	@Email(message = "メール形式が正しくありません。")
-	@Size(min=4,max=30,message="メールアドレスは４文字以上、30文字以下で設定してください。")
+	@Size(min = 4, max = 30, message = "メールアドレスは４文字以上、30文字以下で設定してください。")
 	private String mailAddress;
 	/** パスワード */
 	@NotBlank(message = "パスワードは必須です。")
-	@Size(min=4,max=10,message="パスワードは４文字以上、１０文字以下で設定してください。")
+	@Size(min = 4, max = 10, message = "パスワードは４文字以上、１０文字以下で設定してください。")
 	private String password;
 
 	public String getName() {
