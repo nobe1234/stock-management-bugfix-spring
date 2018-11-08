335d470da54200e1d4ff1bbf2088b44d6d5eef6e
sohei.nobe <sohei.nobe@rakus.co.jp>
2018-11-08 11:26:57 +0900

    (6-1)上級 リポジトリにSQL文追加、コントローラー修正まで　※書籍情報を登録する機能を追加

--- src/main/java/jp/co/rakus/stockmanagement/repository/BookRepository.java
+++ src/main/java/jp/co/rakus/stockmanagement/repository/BookRepository.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Repository;
 
 /**
  * booksテーブル操作用のリポジトリクラス.
+ * 
  * @author igamasayuki
  */
 @Repository
@@ -35,35 +36,36 @@ public class BookRepository {
 		Integer stock = rs.getInt("stock");
 		return new Book(id, name, author, publisher, price, isbncode, saledate, explanation, image, stock);
 	};
-	
+
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
-	
+
 	public List<Book> findAll() {
 		List<Book> books = jdbcTemplate.query(
-				"SELECT id,name,author,publisher,price,isbncode,saledate,explanation,image,stock FROM books order by price", 
+				"SELECT id,name,author,publisher,price,isbncode,saledate,explanation,image,stock FROM books order by price",
 				BOOK_ROW_MAPPER);
 		return books;
 	}
-	
+
 	public Book findOne(Integer id) {
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("id",id);
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 		Book book = jdbcTemplate.queryForObject(
-				"SELECT id,name,author,publisher,price,isbncode,saledate,explanation,image,stock FROM books WHERE id=:id", 
-				param, 
-				BOOK_ROW_MAPPER);
+				"SELECT id,name,author,publisher,price,isbncode,saledate,explanation,image,stock FROM books WHERE id=:id",
+				param, BOOK_ROW_MAPPER);
 		return book;
 	}
-	
+
 	public Book update(Book book) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(book);
 		if (book.getId() == null) {
-			throw new NullPointerException();
-		} 
-		jdbcTemplate.update(
-				"UPDATE books SET stock=:stock WHERE id=:id",
-				param);
+			// TODO:insert文を作成
+			jdbcTemplate.update(
+					"INSERT INTO BOOKS (name,author,publisher,price,isbncode,saledate,explanation,image,stock) "
+							+ "VALUES(:name,:author,:publisher,:price,:isbncode,:saledate,:explanation,:image,:stock)",
+					param);
+		} else {
+			jdbcTemplate.update("UPDATE books SET stock=:stock WHERE id=:id", param);
+		}
 		return book;
 	}
 }
--- src/main/java/jp/co/rakus/stockmanagement/web/BookController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/BookController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 書籍関連処理を行うコントローラー.
+ * 
  * @author igamasayuki
  *
  */
@@ -24,37 +25,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/book")
 @Transactional
 public class BookController {
-	
+
 	@Autowired
 	private BookService bookService;
-	
+
 	/**
 	 * フォームを初期化します.
+	 * 
 	 * @return フォーム
 	 */
 	@ModelAttribute
 	public BookForm setUpForm() {
 		return new BookForm();
 	}
-	
+
 	/**
 	 * 書籍リスト情報を取得し書籍リスト画面を表示します.
+	 * 
 	 * @param model モデル
 	 * @return 書籍リスト表示画面
 	 */
 	@RequestMapping(value = "list")
 	public String list(Model model) {
 		List<Book> bookList = bookService.findAll();
-		
+
 		model.addAttribute("bookList", bookList);
 		return "book/list";
 	}
-	
+
 	/**
 	 * 書籍詳細情報を取得し書籍詳細画面を表示します.
+	 * 
 	 * @param id 書籍ID
-	 * @param model　モデル
-	 * @return　書籍詳細画面
+	 * @param    model モデル
+	 * @return 書籍詳細画面
 	 */
 	@RequestMapping(value = "show/{bookId}")
 	public String show(@PathVariable("bookId") Integer id, Model model) {
@@ -62,13 +66,14 @@ public class BookController {
 		model.addAttribute("book", book);
 		return "book/show";
 	}
-	
+
 	/**
 	 * 書籍更新を行います.
-	 * @param form フォーム
+	 * 
+	 * @param form   フォーム
 	 * @param result リザルト情報
-	 * @param model　モデル
-	 * @return　書籍リスト画面
+	 * @param        model モデル
+	 * @return 書籍リスト画面
 	 */
 	@RequestMapping(value = "update")
 	public String update(@Validated BookForm form, BindingResult result, Model model) {
@@ -76,8 +81,15 @@ public class BookController {
 			return show(form.getId(), model);
 		}
 		Book book = bookService.findOne(form.getId());
+		if (form.getId() == null) {
+			// TODO:登録フォームの作成
+			// TODO:登録後のリンク先は書籍リスト画面でいいか？
+			bookService.update(book);
+			return list(model);
+		}
 		book.setStock(form.getStock());
 		bookService.update(book);
+
 		return list(model);
 	}
 
